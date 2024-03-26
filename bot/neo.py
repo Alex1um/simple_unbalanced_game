676edba36be3b120fa3cbac3cb60a3b7a0db5a8a
Alex1um <ye4h@pm.me>
2024-03-26 17:51:44 +0700

    added neo bot

---
+++ bot/neo.py
@@ -0,0 +1,63 @@
+from websockets.client import connect
+import asyncio
+import signal
+import json
+import numpy as np
+import sys
+import math
+
+TURN_RATE = 10
+SCAN_RANGE = 3
+
+async def play(address: str):
+    async with connect(address) as websocket:
+                # Close the connection when receiving SIGTERM.
+        loop = asyncio.get_running_loop()
+        loop.add_signal_handler(
+            signal.SIGTERM, loop.create_task, websocket.close())
+        try:
+            frame = 0
+            async for data in websocket:
+                frame += 1
+                self_id, ships, bullets, map, killfeed = json.loads(data)
+                self_id = str(self_id)
+                self_ship = ships.get(self_id, None)
+                if self_ship is None: # dead or not in game
+                    await websocket.send(json.dumps({"MoveShip": {"angle": 0.0}}))
+                    continue
+                if frame % TURN_RATE == 0:
+                    observation = np.array(map)
+                    x, y = round(self_ship["x"]) % observation.shape[1], round(self_ship["y"]) % observation.shape[0]
+                    ids = observation[np.ix_(np.arange(y - SCAN_RANGE, y + SCAN_RANGE + 1, 1) % observation.shape[0], np.arange(x - SCAN_RANGE, x + SCAN_RANGE + 1, 1) % observation.shape[1])].flatten()
+                    bullets = tuple(bullets[str(-bid)] for bid in ids[ids < 0])
+                    if bullets:
+                        bullet_coords = np.array(tuple((bullet["x"], bullet["y"]) for bullet in bullets))
+                        bullets_x, bullets_y = bullet_coords[:, 0], bullet_coords[:, 1]
+                        dx = -bullets_x + self_ship["x"]
+                        dy = -bullets_y + self_ship["y"]
+                        angle = np.arctan2(dy, dx)
+                        await websocket.send(json.dumps({"MoveShip": {"angle": angle.mean()}}))
+                    # angle = None
+                    # angle_count = 0
+                    # for id in ids.flatten():
+                    #     if id < 0:
+                    #         bullet = bullets[str(-id)]
+                    #         if angle is None:
+                    #             angle = math.atan2(-bullet["y"] + self_ship["y"], -bullet["x"] + self_ship["x"])
+                    #         else:
+                    #             angle += math.atan2(-bullet["y"] + self_ship["y"], -bullet["x"] + self_ship["x"])
+                    #         angle_count += 1
+                    # if angle is not None:
+                    #     await websocket.send(json.dumps({"MoveShip": {"angle": angle.mean()}}))
+        finally:
+            await websocket.close()
+
+async def multiple_play(count: int):
+    await asyncio.gather(*(play("ws://localhost:48666") for _ in range(count)))
+
+if __name__ == "__main__":
+    count = 1
+    if len(sys.argv) > 1:
+        count = int(sys.argv[1])
+    asyncio.run(multiple_play(count))
+    
