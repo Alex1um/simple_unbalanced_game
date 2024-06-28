4b84b35f026fef7b12715616afdfaa11864551b0
Alex1um <ye4h@pm.me>
2024-06-28 23:35:42 +0700

    added lua script

--- src/scripts/main.lua
+++ src/scripts/main.lua
@@ -1,6 +1,9 @@
-for k, v in pairs(ships) do
-    print(k, ":")
-    for kv, vv in pairs(v) do
-        print("  ", kv, vv)
+for id, ship in pairs(ships) do
+    -- for kv, vv in pairs(v) do
+    --     print("  ", kv, vv)
+    -- end
+    if ship.current_angle ~= ship.angle then
+        ship.x = ship.x + math.cos(ship.current_angle) * ship.v
+        ship.y = ship.y + math.sin(ship.current_angle) * ship.v
     end
 end
